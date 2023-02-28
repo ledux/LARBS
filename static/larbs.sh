c22a7cbb8da7b764c413920c257788cacb4553e5
Lukas Tanner <lukastanner@outlook.com>
2023-02-28 17:37:01 +0100

    chown whole config dir

--- static/larbs.sh
+++ static/larbs.sh
@@ -212,7 +212,7 @@ vimplugininstall() {
 	whiptail --infobox "Installing neovim plugins..." 7 60
 	mkdir -p "/home/$name/.config/nvim/autoload"
 	curl -Ls "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" >  "/home/$name/.config/nvim/autoload/plug.vim"
-	chown -R "$name:wheel" "/home/$name/.config/nvim"
+	chown -R "$name:wheel" "/home/$name/.config/"
 	sudo -u "$name" nvim -c "PlugInstall|q|q"
 }
 
